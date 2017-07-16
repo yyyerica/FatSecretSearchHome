b83e145ffbe33456b3055e0dfb95d5e7d2a228c3
yyyerica <yyyerica@163.com>
2017-07-16 10:09:25 +0800

    初步实现接口
    获得列表中的第一项

---
+++ JsonTest/app/src/main/java/com/example/yyy/jsontest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.yyy.jsontest;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
+
+public class MainActivity extends AppCompatActivity {
+    OkHttpClient client = new OkHttpClient();  //新建客户端
+    JSONObject obj;
+    JSONArray jsonArray = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Log.e("object",FatSecretGet.getFood((long)4889).toString());
+                Log.e("search",FatSecretSearch.searchFood("rice",0).toString());
+                //obj = JSONObject.fromObject(FatSecretSearch.searchFood("banana",0));
+                try {
+                    jsonArray = new JSONArray(FatSecretSearch.searchFood("rice",0).toString());
+                    for (int i = 0; i < 1;i++) {
+                        JSONObject jsonObject = jsonArray.optJSONObject(i);
+                        String food_description = jsonObject.optString("food_description");
+                        //int age = jsonObject.optInt("age");
+                        Log.e("eee",food_description);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
+
+
+    }
+
+
+}
+
+
